1eb1b063c7ec6b4f708902984dd681f55e60ca41
Ray <tanxi3003@gmail.com>
2025-02-21 16:02:20 -0500

    Swerve module made

--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -1,12 +1,18 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class SwerveModule extends SubsystemBase {
+    private final SparkMax driveSparkMax;
+    private final SparkMax turningSparkMax;
     
-}
-//hehehehehe
-//dsfasfgfgs
-//asssjajg
+    public SwerveModule(int driveId, int turningId, double anuglarOffset){
+        driveSparkMax = new SparkMax(driveId, MotorType.kBrushless);
+        turningSparkMax = new SparkMax(turningId, MotorType.kBrushless);
 
-//monkeys like bananas
+        
+    }
+}
